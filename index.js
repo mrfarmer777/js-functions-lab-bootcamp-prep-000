2992d78bf9b3b1cd443786484270a5a469a8120e
mrfarmer777@gmail.com <mrfarmer777@gmail.com>
2018-03-10 16:04:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function happyHolidayTo(holiday,name){
   return `Happy ${holiday}, ${name}!`;
 }
 
-function holidayCountdown(holidday,days){
+function holidayCountdown(holiday,days){
   return `It's ${days} until ${holiday}!`;
 }
 
